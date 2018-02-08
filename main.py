0604672675a6e11cbde95730aede6b642f0b7e02
Chih-Wei Ning <b04902079@ntu.edu.tw>
2018-02-08 14:46:29 +0800

    Able to add users

---
+++ main.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+# 設定資料庫位置，並建立 app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
---
+++ models.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from main import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    questions = db.relationship(
+        'Question',
+        backref='user', # ref 可以讓我們使用 Question.user 進行對 User 操作
+        lazy='dynamic' # 有使用才載入，提昇效能
+    )
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(80))
+    picture_url = db.Column(db.String(80))
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+
+    def __init__(self, content, picture_url):
+        self.description = description
+        self.picture_url = picture_url
+
+    def __repr__(self):
+        return '<Question %r %r>' % self.description % self.picture_url
