cd6c255e3c66a35bf35deaa7d3106334e2c4dd49
Chih-Wei Ning <b04902079@ntu.edu.tw>
2018-02-08 15:24:22 +0800

    Add several questions for a user

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, Server
-from main import app
-from models import User
+from main import app, db
+from models import User, Question
 
 # 設定你的 app
 manager = Manager(app)
@@ -10,7 +10,7 @@ manager.add_command('runserver', Server())
 # 設定 python manage.py shell 為啟動互動式指令 shell 的指令 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, User=User)
+    return dict(app=app, db=db, User=User, Question=Question)
 
 if __name__ == '__main__':
     manager.run()
--- models.py
+++ models.py
@@ -17,15 +17,16 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.name
 
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(80))
     picture_url = db.Column(db.String(80))
-    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, content, picture_url):
+    def __init__(self, description, picture_url):
         self.description = description
         self.picture_url = picture_url
 
     def __repr__(self):
-        return '<Question %r %r>' % self.description % self.picture_url
+        return '<Question %r %r>' % ( self.description, self.picture_url)
