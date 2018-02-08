dfc9a11bb7c0885fe338e178099562d09e5b2a8b
Chih-Wei Ning <b04902079@ntu.edu.tw>
2018-02-08 15:43:14 +0800

    Use flask-migrate

--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 from main import app, db
 from models import User, Question
 
-# 設定你的 app
 manager = Manager(app)
-# 設定 python manage.py runserver 為啟動 server 指令
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server())
 
-# 設定 python manage.py shell 為啟動互動式指令 shell 的指令 
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db, User=User, Question=Question)
