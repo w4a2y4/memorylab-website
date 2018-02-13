87a29e5038cdc7f2d939461f99093a29b2fc323c
Chih-Wei Ning <b04902079@ntu.edu.tw>
2018-02-13 15:30:12 +0800

    init db

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 
-from models import User, Question
+from models import User, Question, db
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db = SQLAlchemy(app)
 admin = Admin(app)
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Question, db.session))
@@ -20,4 +19,7 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    db.app = app
+    db.init_app(app)
+    db.create_all()
     app.run()
