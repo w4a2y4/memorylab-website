411082f74d5779144bebd536205a6fbb0893146c
Chih-Wei Ning <b04902079@ntu.edu.tw>
2018-03-11 17:52:48 +0800

    Fade

---
+++ static/js/common.js
@@ -0,0 +1,10 @@
+let threshold = $('#header').height();
+
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > threshold ) {
+        if ($('#fixed_head').is(':hidden'))
+            $('#fixed_head').fadeIn(200);
+    }
+    else if ($('#fixed_head').css('opacity') > 0)
+        $('#fixed_head').fadeOut(200);
+});
